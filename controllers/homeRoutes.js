5e74226a717fa79e5e82a679384fc552c1070309
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-05-12 19:48:42 -0400

    profile handlebars updated.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,4 +8,12 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/profile', async (req, res) => {
+    try {
+        res.render('profile');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 module.exports = router;
