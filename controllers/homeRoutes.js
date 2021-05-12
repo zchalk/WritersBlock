ff0634f47be46a9ed851f37697bfa7f6b75e347b
Broc C <bac5806@gmail.com>
2021-05-12 19:53:54 -0400

    added random functions from pulling prompts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { Prompts } = require('../models');
 const withAuth = require('../utils/auth');
+let getRandomInt = require('../utils/random');
+const sequelize = require('../config/connection');
 
 router.get('/', async (req, res) => {
     try {
@@ -11,14 +13,17 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/prompt/:id', async (req, res) => {
+    
+    const [results, metadata] = await sequelize.query("SELECT COUNT(*) FROM prompts;");
+    let randNum = getRandomInt(results[0]['COUNT(*)']);
+
     try{ 
-        const promptData = await Prompts.findByPk(req.params.id);
+        const promptData = await Prompts.findByPk(randNum);
         if(!promptData) {
             res.status(404).json({message: 'No prompt with this id!'});
             return;
         }
         const p = promptData.get({ plain: true });
-        console.log(p);
         res.render('prompt', p);
       } catch (err) {
           res.status(500).json(err);
---
+++ utils/random.js
@@ -0,0 +1,6 @@
+const getRandomInt = (max) => {
+    console.log(max);
+    return Math.floor(Math.random() * max + 1);
+  }
+
+  module.exports = getRandomInt;
