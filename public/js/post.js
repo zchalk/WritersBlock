b1cdbb072a1d67bc83460123648a263dc8083fc7
Broc C <bac5806@gmail.com>
2021-05-13 13:16:06 -0400

    can add blogs from post route to database

--- public/js/post.js
+++ public/js/post.js
@@ -3,17 +3,16 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
   
     // Gather the data from the form elements on the page
-    const email = document.querySelector('#exampleInputEmail1').value.trim();
+    const blog = document.querySelector('#blog-input').value.trim();
 
-    // Send the e-mail and password to the server
     fetch('/api/blogposts', {
     method: 'POST',
-    body: JSON.stringify({ email }),
+    body: JSON.stringify({ blog }),
     headers: { 'Content-Type': 'application/json' },
     });
   };
   
   document
-    .querySelector('.login-form')
+    .querySelector('.blog-form')
     .addEventListener('submit', loginFormHandler);
   
