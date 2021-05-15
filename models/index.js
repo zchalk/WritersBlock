c70ac332009b31974c15ad08aaddc4e6100ae865
Broc C <bac5806@gmail.com>
2021-05-15 13:13:58 -0400

    commting

--- models/index.js
+++ models/index.js
@@ -2,14 +2,14 @@ const Prompts = require('./prompts');
 const User = require('./user');
 const Blogs = require('./blogs');
 
-// User.hasMany(Blogs, {
-//     foreignKey: 'user_id',
-//     foreignKey: 'prompts_id',
-//     onDelete: 'CASCADE'
-// });
+User.hasMany(Blogs, {
+    foreignKey: 'user_id',
+    foreignKey: 'prompts_id',
+    onDelete: 'CASCADE'
+});
 
-// Blogs.belongsTo(User, {
-//     foreignKey: 'user_id'
-// });
+Blogs.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
 module.exports = {Prompts, User, Blogs};
